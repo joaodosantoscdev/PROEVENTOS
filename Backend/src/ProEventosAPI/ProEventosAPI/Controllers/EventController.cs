0b06ed2ae082c4e43857db0e0cdabb4dd4005e75
João Vítor Carvalho <mistingerr@gmail.com>
2021-12-14 19:56:57 -0300

    adjust files

---
+++ Backend/src/ProEventosAPI/ProEventosAPI/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+
+        }
+    }
+}

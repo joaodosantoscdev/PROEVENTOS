681ec3dd2534dbd2c6839c6c4340ac919c4c663e
João Vítor Carvalho <mistingerr@gmail.com>
2021-12-14 21:28:42 -0300

    added-context/initialized controller developmnet

--- Backend/src/ProEventosAPI/ProEventosAPI/Controllers/EventController.cs
+++ Backend/src/ProEventosAPI/ProEventosAPI/Controllers/EventController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventosAPI.Data;
+using ProEventosAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,18 +10,31 @@ using System.Threading.Tasks;
 namespace ProEventosAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class EventController : ControllerBase
     {
-        public EventController()
+        // Constructor &  Dependencies
+        #region DI Injected
+        private readonly DataContext _context;
+        public EventController(DataContext context)
         {
-            
+            _context = context;
         }
+        #endregion
 
+        // GET - Events
+        #region GET Methods
         [HttpGet]
         public IEnumerable<Event> Get()
         {
-
+            return _context.Events;
         }
+
+        [HttpGet("{id}")]
+        public Event GetById(int id) 
+        {
+            return _context.Events.FirstOrDefault(e => e.Id == id);
+        }
+        #endregion
     }
 }
---
+++ Backend/src/ProEventosAPI/ProEventosAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventosAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventosAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}

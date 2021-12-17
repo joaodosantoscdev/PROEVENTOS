c6ef4e51b5ed366fc75555b3a48d8e6283b0fa0c
João Vítor Carvalho <mistingerr@gmail.com>
2021-12-16 21:08:31 -0300

    added-correct versions and tested

--- Frontend/ProEventos-App/src/app/app.module.ts
+++ Frontend/ProEventos-App/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-      SpeakersComponent
-   ],
+    SpeakersComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

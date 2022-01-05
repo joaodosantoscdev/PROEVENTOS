333b32e2575b2dcaaafbfa2020f95969de58f8b0
João Vítor Carvalho <mistingerr@gmail.com>
2022-01-05 17:03:49 -0300

    layout improvements | events

--- Frontend/ProEventos-App/src/app/shared/title/title.component.ts
+++ Frontend/ProEventos-App/src/app/shared/title/title.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
  @Input() title: string;
+ @Input() iconClass = 'fa fa-user';
+ @Input() subtitle = 'A melhor plataforma de eventos';
+ @Input() btnList = false;
 
   constructor() { }
 

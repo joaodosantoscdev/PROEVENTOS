c3c59cb6219b6e630a2ecfd5f7d5ac492620e5c5
João Vítor Carvalho <mistingerr@gmail.com>
2022-01-05 12:37:26 -0300

    angular updated v11

--- Frontend/ProEventos-App/src/app/helpers/DateFormat.pipe.spec.ts
+++ Frontend/ProEventos-App/src/app/helpers/DateFormat.pipe.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { DateFormatPipe } from './DateFormat.pipe';
 
 describe('Pipe: DateFormate', () => {

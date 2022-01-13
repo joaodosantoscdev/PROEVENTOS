19bec33bc715389e39846053d308a9e29024121a
João Vítor Carvalho <mistingerr@gmail.com>
2022-01-13 11:46:47 -0300

    Part Controllers[API layer], Services[Application layer], Repos[Persistence layer] | Backend

---
+++ Backend/src/ProEventosAPI/ProEventos.Application/Services/Interfaces/IPartService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using ProEventos.Application.DTO;
+
+namespace ProEventos.Application.Services.Interfaces
+{
+    public interface IPartService
+    {
+        Task<PartDTO[]> SaveParts(int eventId, PartDTO[] models);
+        Task<bool> DeletePart(int eventId, int id);
+        Task<PartDTO[]> GetAllPartsByEventIdAsync(int eventId);
+        Task<PartDTO> GetPartByIdsAsync(int eventId, int id);
+    }   
+}

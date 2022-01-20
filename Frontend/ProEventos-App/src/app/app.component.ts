6c09fdc34da98ca70415337d5b745ace24a734e9
João Vítor Carvalho <mistingerr@gmail.com>
2022-01-20 17:30:38 -0300

    added restrict routes by auth-user, fixed nav bugs and added route-guard | Frontend

--- Frontend/ProEventos-App/src/app/app.component.ts
+++ Frontend/ProEventos-App/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './models/identity/User';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProEventos-App';
+
+  constructor(public userService: UserService) { }
+
+   // tslint:disable-next-line: use-lifecycle-interface
+   ngOnInit(): void {
+    this.setCurrentUser();
+   }
+
+   setCurrentUser(): void {
+    let user: User;
+
+    if (localStorage.getItem('user')) {
+      user = JSON.parse(localStorage.getItem('user') ?? '{}');
+    } else {
+      user = null;
+    }
+
+    if (user) {
+      this.userService.setCurrentUser(user);
+    }
+   }
 }

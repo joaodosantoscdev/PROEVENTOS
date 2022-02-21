3ab910df59165fe7f8f739ac8ce91f9494b94e02
João <mistingerr@gmail.com>
2022-02-21 10:20:06 -0300

    speakers-sm-links

--- Frontend/ProEventos-App/src/app/components/speakers/speakers-list/speakers-list.component.ts
+++ Frontend/ProEventos-App/src/app/components/speakers/speakers-list/speakers-list.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 
 import { PaginatedResult, Pagination } from '@app/models/pagination';
 import { Speaker } from '@app/models/Speaker';
+import { SocialMedia } from '@app/models/SocialMedia';
+import { SocialMediaService } from '@app/services/socialMedia.service';
 import { SpeakerService } from '@app/services/speaker.service';
 import { environment } from '@environments/environment';
 
@@ -12,6 +14,7 @@ import { ToastrService } from 'ngx-toastr';
 
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
+import { cwd } from 'process';
 
 @Component({
   selector: 'app-speakers-list',
@@ -21,7 +24,7 @@ import { debounceTime } from 'rxjs/operators';
 export class SpeakersListComponent implements OnInit {
 
   public speakers: Speaker[] = [];
-  public speakerId = 0;
+  public socialMedias = [];
   public pagination = {} as Pagination;
 
   termSearchChange: Subject<string> = new Subject<string>();
@@ -34,10 +37,12 @@ export class SpeakersListComponent implements OnInit {
     private router: Router,
   ) { }
 
-
-
   ngOnInit(): void {
     this.loadSpeakers();
+  }
+
+  public returnTitle(name: string): string {
+    return name === null || name === '' ? null : name;
   }
 
   public loadSpeakers(): void {
@@ -48,6 +53,7 @@ export class SpeakersListComponent implements OnInit {
         (paginatedResult: PaginatedResult<Speaker[]>) => {
           this.speakers = paginatedResult.result;
           this.pagination = paginatedResult.pagination;
+          debugger;
         },
         (error: any) => {
           this.spinner.hide();
@@ -56,6 +62,13 @@ export class SpeakersListComponent implements OnInit {
       )
       .add(() => this.spinner.hide());
   }
+
+  public getSpeakersSocialMedia(): object {
+    this.socialMedias =  this.speakers
+      .map((speaker) => speaker.socialMedias.map((sm, i) => sm[i].name));
+    return this.socialMedias;
+  }
+
 
   public filterSpeakers(speaker: any): void {
     if (this.termSearchChange.observers.length === 0 ) {
